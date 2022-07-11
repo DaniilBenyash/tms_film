e8e171370a47365c3919ed5eb07cf4247541f72a
DaniilBenyash <daniilbenyash22@gmail.com>
2022-07-11 15:36:42 +0300

    fix: adaptive

--- src/components/User/components/UserMain/UserMain.tsx
+++ src/components/User/components/UserMain/UserMain.tsx
@@ -15,6 +15,8 @@ export const UserMain = () => {
     return (
         <div className="user-main">
             <Link to='/settings' className="user-main__button user-main__button_top">Edit Profile</Link>
+            <Link to='/trends' className="user-main__button user-main__button_center">Trends</Link>
+            <Link to='/favorite' className="user-main__button user-main__button_center">Favorite</Link>
             <button className="user-main__button user-main__button_bottom" onClick={logOut}>Log Out</button>
         </div>
     )
